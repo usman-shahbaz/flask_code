6d2d49930f897f64a31de41e4bc3e6e88538893d
usman-shahbaz <ua5979800@gmail.com>
2026-04-30 22:05:09 +0500

    added CI/CD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,12 @@
+import sys
+import os
 import json
+
+# ✅ Add project root to path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
+
 
 def test_home():
     client = app.test_client()
