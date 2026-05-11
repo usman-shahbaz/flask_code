abe53615b7b063cd37e725ffde9e372543baa530
Usman_Shahbaz <122297454+usman-shahbaz@users.noreply.github.com>
2026-05-11 23:57:22 +0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ import sys
 import os
 import json
 
-# ✅ Add project root to path
+# ✅Add project root to path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app
